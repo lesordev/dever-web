ddb62fa9a36c85c4a311cfc14d4968785fa20706
luc.le <trungluc.dev@gmail.com>
2022-09-12 08:43:39 +0700

    remove unused code

--- src/shared/components/sector-card/SectorCard.tsx
+++ src/shared/components/sector-card/SectorCard.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { useMotionValue, useTransform, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 interface Props {
   className?: string;
